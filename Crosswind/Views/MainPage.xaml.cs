bb1fe5ce1129eeeb5a0aebdd34e324e6fa209e98
Ryan Clarke <ryan@ryanclarke.net>
2014-08-29 06:29:27 -0400

    Clear back stack up to start of wizard on arriving at MainPage

--- Crosswind/Views/MainPage.xaml.cs
+++ Crosswind/Views/MainPage.xaml.cs
@@ -18,9 +18,6 @@ namespace Crosswind
         public MainPage()
         {
             InitializeComponent();
-
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
         }
 
         private void Runway_Tap(object sender, System.Windows.Input.GestureEventArgs e)
@@ -46,32 +43,31 @@ namespace Crosswind
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            base.OnNavigatedTo(e);
+
             model = UrlService.ExtractModelFromQueryString(NavigationContext.QueryString);
 
+            SetupView();
+
+            ClearBackStackToStartOfWizard();
+        }
+
+        private void SetupView()
+        {
             RunwayNumber.Text = model.Runway;
             WindHeadingNumber.Text = model.WindHeading;
             WindSpeedNumber.Text = model.WindSpeed;
             CrosswindNumber.Text = model.CrosswindSpeed();
             CrosswindDirection.Text = model.CrosswindDirection();
             CrosswindDirectionBlock.Visibility = model.ShouldCrosswindDirection() ? Visibility.Visible : Visibility.Collapsed;
-
-            base.OnNavigatedTo(e);
         }
 
-        // Sample code for building a localized ApplicationBar
-        //private void BuildLocalizedApplicationBar()
-        //{
-        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
-        //    ApplicationBar = new ApplicationBar();
-
-        //    // Create a new button and set the text value to the localized string from AppResources.
-        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
-        //    appBarButton.Text = AppResources.AppBarButtonText;
-        //    ApplicationBar.Buttons.Add(appBarButton);
-
-        //    // Create a new menu item with the localized string from AppResources.
-        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
-        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
-        //}
+        private void ClearBackStackToStartOfWizard()
+        {
+            while (NavigationService.BackStack.Count() > 1)
+            {
+                NavigationService.RemoveBackEntry();
+            }
+        }
     }
 }
